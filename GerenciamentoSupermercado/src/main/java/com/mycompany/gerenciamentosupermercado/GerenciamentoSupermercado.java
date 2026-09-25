6402eae3bf47ac20d888b3f37e1e2835a4d32420
JhessikKaellyLeal <jhessikkaelly.bezerraleal@gmail.com>
2026-09-25 16:41:49 -0300

    criação do projeto em java

---
+++ GerenciamentoSupermercado/src/main/java/com/mycompany/gerenciamentosupermercado/GerenciamentoSupermercado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gerenciamentosupermercado;
+
+/**
+ *
+ * @author JhéssikLeal
+ */
+public class GerenciamentoSupermercado {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
